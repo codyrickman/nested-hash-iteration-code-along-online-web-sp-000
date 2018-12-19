b1c7cc2a350802ddb00a995564ca18a75536239e
Cody Rickman <cody@creativemindstudios.com>
2018-12-19 17:25:33 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,7 @@ def remove_strawberry(contacts)
   contacts.each do |contact, values|
     values.each do |label, value|
       if (value.include? 'strawberry')
-        contacts[contact][label] - ['strawberry']
+        contacts[contact][label].delete('strawberry')
       end
     end
   end
